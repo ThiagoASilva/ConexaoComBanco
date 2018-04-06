60443c84235f9c711bde1633f99b87d70b8cae2c
Aluno <Aluno@LAB-06-15>
2018-04-05 22:45:51 -0300

    Acrencentado a função listar

--- ConexaoComBanco/src/persistence/AlunoJdbcDAO.java
+++ ConexaoComBanco/src/persistence/AlunoJdbcDAO.java
@@ -2,7 +2,10 @@ package persistence;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import model.Aluno;
 
@@ -21,5 +24,29 @@ public class AlunoJdbcDAO {
 		PreparedStatement prepareStatement = this.conn.prepareStatement(sql);
 		prepareStatement.executeUpdate();
 		prepareStatement.close();
+	}
+	
+	public List<Aluno> listar() {
+		String sql = "select * from alunos";
+        System.out.println(sql);		
+        List<Aluno> Aluno = new ArrayList<Aluno>();
+		try {
+			PreparedStatement prepareStatement = this.conn.prepareStatement(sql);
+			ResultSet rs = prepareStatement.executeQuery();
+			while(rs.next()) {				
+				String nome = rs.getString("nome");
+				String endereco = rs.getString("endereco");
+				String bairro = rs.getString("bairro");				
+				Aluno aluno = new Aluno();				
+				aluno.setNome(nome);
+				aluno.setEndereco(endereco);
+				aluno.setBairro(bairro);
+				
+			}
+			prepareStatement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return Aluno;
 	}
 }
